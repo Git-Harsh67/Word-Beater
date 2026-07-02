30605b9c2e77254a6a0a37a32382285f1a7df260
Harsh <harsharyahi@gmail.com>
2026-07-02 19:09:55 +0530

    second commit

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const startbtn = document.querySelector(".btn")
 const homePage = document.querySelector("#homePage") 
+const mainPage = document.querySelector("#mainPage") 
 
 startbtn.addEventListener("click", ()=>{
     console.log("clicked")
     homePage.style.display = "none"
+    mainPage.style.display = "flex"
 })
 
 
